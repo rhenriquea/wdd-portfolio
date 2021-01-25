934b93f4fbec1e4dabb24043e0cd17dd36a63ea1
rhenriquea <rafael.dealmeida@jumbo.com>
2021-01-25 20:02:54 +0100

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,24 @@
+export default {
+  ssr: false,
+  target: 'static',
+  head: {
+    title: 'wdd-portfolio',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  components: true,
+
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/stylelint-module',
+    '@nuxtjs/tailwindcss',
+  ],
+}

a9ae3e7d7cb872ae7020f730b3ae6aa30af7c70f
rhenriquea <rafael.dealmeida@jumbo.com>
2021-02-04 19:25:27 +0100

    first todo commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
+    <h2>Readings</h2>
     <ol class="nav-links">
       <li class="mr" :class="{ 'mr-0': index === links.length - 1 }" v-for="(link, index) in links">
+        <NuxtLink :to="link.to">{{ link.name }}</NuxtLink>
+      </li>
+    </ol>
+    <hr class="mt-2x" />
+    <h2>Applications</h2>
+    <ol class="nav-links">
+      <li
+        class="mr"
+        :class="{ 'mr-0': index === links.length - 1 }"
+        v-for="(link, index) in applications"
+      >
         <NuxtLink :to="link.to">{{ link.name }}</NuxtLink>
       </li>
     </ol>
@@ -18,6 +30,7 @@ export default {
       { name: 'Week 04', to: '/week4' },
       { name: 'Week 05', to: '/week5' },
     ],
+    applications: [{ name: 'ToDo App', to: '/todo' }],
   }),
 };
 </script>
---
+++ pages/todo/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>TODOS</h2>
+
+    <ul>
+      <li v-for="todo in todos" :key="todo.id" :class="{ completed: todo.completed }">
+        <input type="checkbox" :checked="todo.completed" />
+        <h4>{{ todo.content }}</h4>
+        <small>{{ todo.id }}</small>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    todos: [
+      { id: new Date(), content: 'Some dummy content', completed: false },
+      { id: new Date(), content: 'Another dummy content', completed: true },
+    ],
+  }),
+};
+</script>
